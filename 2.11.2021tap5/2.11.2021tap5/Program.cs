743996a1f6f80fcc4ca43f9a357db45d2cf653d0
10hesen044 <tu201906250@code.edu.az>
2021-11-02 11:51:04 +0400

    project added

---
+++ 2.11.2021tap5/2.11.2021tap5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _2._11._2021tap5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CreatMethod("Aze","Baku","Uni"));
+        }
+
+        static string CreatMethod(params string[] mass)
+        {
+            string strArr = string.Empty;
+            
+            for (int i = 0; i<mass.Length; i++)
+			{
+               strArr = mass[i];
+			}
+             return strArr;
+        }    
+    }
+}
